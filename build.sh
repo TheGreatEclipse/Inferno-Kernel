50250209180520cd86f66c169cbfbb330d04cabc
The Great Eclipse <greateclipse609@gmail.com>
2026-04-26 10:55:34 +0400

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,75 +1,92 @@
 #!/usr/bin/env bash
 #
-SECONDS=0
-ZIPNAME="Inferno-Ginkgo-$(TZ=Asia/Baku date +"%Y%m%d-%H%M").zip"
-TC_DIR="/home/xmoon/Documents/GINKGO/Kernel_Build/Toolchain/"
-CLANG_DIR="${TC_DIR}clang-r498229b"
-GCC_64_DIR="${TC_DIR}android_prebuilts_gcc_linux-x86_aarch64_aarch64-linux-android-4.9"
-GCC_32_DIR="${TC_DIR}android_prebuilts_gcc_linux-x86_arm_arm-linux-androideabi-4.9"
-AK3_DIR="$(pwd)/AnyKernel3"
+
+SECONDS=0 # builtin bash timer
+ZIPNAME="Destruction-Ginkgo-$(TZ=Asia/Baku date +"%Y%m%d-%H%M").zip"
+TC_DIR="$(pwd)/../tc/"
+CLANG_DIR="${TC_DIR}clang"
+GCC_64_DIR="${TC_DIR}aarch64-linux-android-4.9"
+GCC_32_DIR="${TC_DIR}arm-linux-androideabi-4.9"
+AK3_DIR="$(pwd)/../AnyKernel3"
 DEFCONFIG="vendor/ginkgo_defconfig"
 
-# ===== Set timezone =====
-export TZ=Asia/Jakarta
-
-
-# ===== ENV =====
 export PATH="$CLANG_DIR/bin:$PATH"
 export LD_LIBRARY_PATH="$CLANG_DIR/lib:$LD_LIBRARY_PATH"
 export KBUILD_BUILD_VERSION="1"
 export LOCALVERSION
 
+if ! [ -d "${CLANG_DIR}" ]; then
+    echo "Clang not found! Downloading and extracting to ${CLANG_DIR}..."
+    mkdir -p "${CLANG_DIR}"
+    
+    # Download the specific Clang revision
+    wget -q https://android.googlesource.com/platform/prebuilts/clang/host/linux-x86/+archive/42d100b025eb5f4a41781348016c148e9e912cf9/clang-r498229b.tar.gz -O clang.tar.gz
+    
+    # Extract directly into the Clang directory
+    tar -xf clang.tar.gz -C "${CLANG_DIR}"
+    
+    # Cleanup the tarball to save space
+    rm clang.tar.gz
+    
+    if [ $? -ne 0 ]; then
+        echo "Clang setup failed! Aborting..."
+        exit 1
+    fi
+fi
 
+if ! [ -d "${GCC_64_DIR}" ]; then
+echo "gcc not found! Cloning to ${GCC_64_DIR}..."
+if ! git clone --depth=1 -b lineage-19.1 https://github.com/LineageOS/android_prebuilts_gcc_linux-x86_aarch64_aarch64-linux-android-4.9.git ${GCC_64_DIR}; then
+echo "Cloning failed! Aborting..."
+fi
+fi
+
+if ! [ -d "${GCC_32_DIR}" ]; then
+echo "gcc_32 not found! Cloning to ${GCC_32_DIR}..."
+if ! git clone --depth=1 -b lineage-19.1 https://github.com/LineageOS/android_prebuilts_gcc_linux-x86_arm_arm-linux-androideabi-4.9.git ${GCC_32_DIR}; then
+echo "Cloning failed! Aborting..."
+fi
+fi
 
 mkdir -p out
 make O=out ARCH=arm64 $DEFCONFIG
 
-# ===== BUILD =====
-echo "🔨 Compilation Started"
+echo -e "\nStarting compilation...\n"
 make -j$(nproc --all) O=out \
-ARCH=arm64 \
-CC=clang \
-LD=ld.lld \
-AR=llvm-ar \
-AS=llvm-as \
-NM=llvm-nm \
-OBJCOPY=llvm-objcopy \
-OBJDUMP=llvm-objdump \
-STRIP=llvm-strip \
-CROSS_COMPILE=aarch64-linux-android- \
-CROSS_COMPILE_ARM32=arm-linux-gnueabi- \
-CLANG_TRIPLE=aarch64-linux-gnu- \
-Image.gz-dtb \
-dtbo.img 2>&1 | tee log.txt
+					  ARCH=arm64 \
+					  CC=clang \
+					  LD=ld.lld \
+					  AR=llvm-ar \
+					  AS=llvm-as \
+					  NM=llvm-nm \
+					  OBJCOPY=llvm-objcopy \
+					  OBJDUMP=llvm-objdump \
+					  STRIP=llvm-strip \
+					  CROSS_COMPILE=aarch64-linux-android- \
+					  CROSS_COMPILE_COMPAT=arm-linux-gnueabi- \
+					  CLANG_TRIPLE=aarch64-linux-gnu- \
+					  Image.gz-dtb \
+					  dtbo.img
 
-# ===== CHECK RESULT =====
 if [ -f "out/arch/arm64/boot/Image.gz-dtb" ] && [ -f "out/arch/arm64/boot/dtbo.img" ]; then
-echo "✅ Build Success"
-echo "Zipping kernel..."
-
+echo -e "\nKernel compiled succesfully! Zipping up...\n"
 if [ -d "$AK3_DIR" ]; then
 cp -r $AK3_DIR AnyKernel3
-else
-git clone -q https://github.com/neophyteprjkt/AnyKernel3
+elif ! git clone -q https://github.com/TheGreatEclipse/AnyKernel3; then
+echo -e "\nAnyKernel3 repo not found locally and cloning failed! Aborting..."
 fi
-
 cp out/arch/arm64/boot/Image.gz-dtb AnyKernel3
 cp out/arch/arm64/boot/dtbo.img AnyKernel3
-
-rm -rf *zip
+rm -f *zip
 cd AnyKernel3
 git checkout main &> /dev/null
 zip -r9 "../$ZIPNAME" * -x '*.git*' README.md *placeholder
 cd ..
-
-echo "📦 Kernel Build Finished"
-echo "⏱ Time: $((SECONDS / 60))m $((SECONDS % 60))s"
-
 rm -rf AnyKernel3
 rm -rf out/arch/arm64/boot
+echo -e "Completed in $((SECONDS / 60)) minute(s) and $((SECONDS % 60)) second(s) !"
+echo "Zip: $ZIPNAME"
 else
-echo "❌ Build Failed"
-echo "Check log.txt"
+echo -e "\nCompilation failed!"
 fi
-
-echo "🎉 Done!"
+echo -e "======================================="
